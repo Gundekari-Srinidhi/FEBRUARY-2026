43e9ce526927c5e21fcefa69ae37d5d113ecd5fb
Gundekari-Srinidhi <gundekarisrinidhi@gmail.com>
2026-02-15 05:25:02 +0530

    Time: 32 ms (93.72%), Space: 19.5 MB (42.85%) - LeetHub

---
+++ 0264-ugly-number-ii/0264-ugly-number-ii.py
@@ -0,0 +1,24 @@
+class Solution:
+    def nthUglyNumber(self, n: int) -> int:
+
+        ugly = [1] * n
+
+        i2 = i3 = i5 = 0
+
+        for i in range(1, n):
+
+            next2 = ugly[i2] * 2
+            next3 = ugly[i3] * 3
+            next5 = ugly[i5] * 5
+
+            nxt = min(next2, next3, next5)
+            ugly[i] = nxt
+
+            if nxt == next2:
+                i2 += 1
+            if nxt == next3:
+                i3 += 1
+            if nxt == next5:
+                i5 += 1
+
+        return ugly[-1]
